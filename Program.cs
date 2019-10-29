a3cc56765f2d16626d95a621cf47808d72d57508
ShaolinSunset <54691867+ShaolinSunset@users.noreply.github.com>
2019-10-29 19:17:39 -0400

    add files via upload
    initial commit with finished code

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Min_max_difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[6];
+
+            numbers[0] = 15;
+            numbers[1] = 22;
+            numbers[2] = 84;
+            numbers[3] = 14;
+            numbers[4] = 88;
+            numbers[5] = 23;
+
+            int highest_value = 88;
+            int smallest_value = 14;
+
+            int difference = highest_value - smallest_value;
+
+            Console.Write(difference);
+
+            Console.ReadLine();
+
+        }
+    }
+}
